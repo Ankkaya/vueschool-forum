ba3afbadd07e7f9aad524ac680cd20cacc0ca766
Ankkaya <staryou@126.com>
2023-06-30 16:49:46 +0800

    [新增] 文章详情页

---
+++ src/components/PageHome.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-full">
+    <h1>welcome to the forum</h1>
+    <ThreadList :threads="threads" />
+  </div>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+import ThreadList from "./ThreadList.vue";
+export default {
+  data() {
+    return {
+      threads: sourceData.threads,
+    };
+  },
+  components: {
+    ThreadList,
+  },
+};
+</script>
+
+<style></style>
